d33ba5ecb8c792db5f5be6ed0b7eead26cd56ba8
damian98g <83282319+damian98g@users.noreply.github.com>
2021-05-14 22:06:48 +0200

    Add files via upload

---
+++ ćw_8.py
@@ -0,0 +1,11 @@
+import geopandas
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+gdf = geopandas.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf.to_crs("EPSG:4326")
+gdf.plot("TOT", legend = True)
+gdf['centroid'] = gdf.centroid
+
